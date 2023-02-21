b06d2b6574aefda4b2eeea346b59df08295dc05c
Sakhe12 <sakhemzolisa@gmail.com>
2023-02-21 16:47:00 +0200

    update

---
+++ config/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { createPool } = require('mysql');
+//XC
+let connection = createPool({
+    host: process.env.dbHost,
+    user: process.env.dbUser,
+    password: process.env.dbPassword,
+    port: process.env.dbPort,
+    database: process.env.dbDatabase,
+    multipleStatements: true
+});
+module.exports = connection;
